4576cd02cbde0d6afdfde16e68b80e651280483a
calvin22580 <hollowaycalvin@gmail.com>
2023-08-17 19:54:42 +0100

    htgere

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # Set the width of the output window
-st.set_page_config(layout="centered", theme="light")
+st.set_page_config(layout="wide")
 
 # Define the URL of the chatbot API
 url = 'https://chatbot-pzcsm.northeurope.inference.ml.azure.com/score'
